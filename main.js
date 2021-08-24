91be4fe3d5aa3e3c4154505ec718e44fd5e12bd5
Asanyth <60246933+Asanyth@users.noreply.github.com>
2021-08-23 22:52:20 -0400

    Added code to animate the dialog. --Completed

--- main.js
+++ main.js
@@ -112,19 +112,38 @@ const mobileOptionButtonPosition = () => {
 
 mobileOptionButtonPosition()
 
-// let text = 'Welcome adventurer. What is your name?'
+// ************************************************************
+// Alternate Version of typing text 1 letter at a time
+// Cache the element
+// const div = document.querySelector('#text');
 
-// // Adds one letter at a time to the adventure dialog box.
-// const textArray = text.split('');
-// let loopTimer;
+// function frameLooper(str) {
 
-// const frameLooper = () => {
-//     textArray.length > 0 ? document.querySelector('#text').innerHTML += textArray.shift() : clearTimeout(loopTimer);
-//     loopTimer = setTimeout('frameLooper()', 30);
+//   // Split the string
+//   const arr = str.split('');
+
+//   // Create a small loop for the `setTimeout` to call
+//   // Set the index to 0
+//   function loop(i = 0) {
+
+//     // If the index is less than the array length
+//     // add a letter from the array
+//     if (i < arr.length) div.textContent += arr[i];
+
+//     // Otherwise increase the index, and run the loop again
+//     setTimeout(loop, 100, ++i);
+
+//   }
+
+//   loop();
+
 // }
 
-// frameLooper()
+// // Pass in the string
+// frameLooper('This is a test');
+// ************************************************************
 
+// Loops thought text
 let timer;
 function typer(text) {
   let characters = text.split('');
